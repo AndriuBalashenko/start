740d2204282a88cf922e878fbb83f5832a6f4dd6
AndriuBalashenko <andriu04081992@gmail.com>
2022-07-26 20:29:07 +0300

    added 3rd event 0.2.0

--- loop.py
+++ loop.py
@@ -1,26 +1,37 @@
 from queue import Queue
 from functools import partial
 
+#петля событий
 event_loop = None
 
 class EventLoop(Queue):
-    def start(self):
-        i=0
-        while True:
-            function = self.get()
-            function()
-            i-=1
-            if i <= 0: break
+  def start(self, limit=-1):
+    while True: #петля
+      function = self.get() #взять из очереди
+      function()
+      limit -= 1
+      if limit ==0: break
+
 def buy_water():
-    global event_loop
-    print("Buy water")
-    event_loop.put(buy_water())
+  global event_loop
+  print("Buying water")
+  event_loop.put(buy_bread) #поставить в очередь
 
 def buy_bread():
-    global event_loop
-    print("Buying bread")
-    event_loop.put(buy_water())
+  global event_loop
+  print("Buying bread")
+  event_loop.put(buy_water) #поставить в очередь
 
+def buy_iphone():
+    global event_loop
+    print("Buying iphone")
+    exit(1)
+    
 event_loop = EventLoop()
 event_loop.put(buy_water)
-event_loop.start()
+event_loop.start(limit=5)
+
+
+
+
+
